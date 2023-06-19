184f47e7c22d0566c0f7aea1d99b811ca3151a10
Jahongir-web <jahongirmh@gmail.com>
2023-06-19 11:42:49 +0500

    fixed cloudinary error

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(fileUpload());
+app.use(fileUpload({ useTempFiles: true }));
 
 app.use(ErrorController);
 
